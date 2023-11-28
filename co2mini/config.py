0d1c1066fcce6ea1e0637ed7b31d6e378df44224
Jeremy Mayeres <jeremy@jerr.dev>
2023-11-28 22:39:45 +0200

    Add MQTT support for HA

---
+++ co2mini/config.py
@@ -0,0 +1,21 @@
+import platform
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+HOSTNAME = platform.node()
+
+PROMETHEUS_PORT: int = env.int("CO2_PROMETHEUS_PORT", 9999)
+NAME: str = env.str("CO2MINI_NAME", "co2mini")
+
+with env.prefixed("MQTT_"):
+    MQTT_ENABLED: bool = env.bool("ENABLED", False)
+    MQTT_BROKER: str = env.str("BROKER", "localhost")
+    MQTT_PORT: str = env.int("PORT", 1883)
+    MQTT_USERNAME: str = env.str("USERNAME", "")
+    MQTT_PASSWORD: str = env.str("PASSWORD", "")
+    MQTT_DISCOVERY_PREFIX: str = env.str("DISCOVERY_PREFIX", "homeassistant")
+    # Object ID needs to be unique
+    MQTT_OBJECT_ID: str = env.str("OBJECT_ID", f"co2mini_{HOSTNAME}")
---
+++ co2mini/mqtt.py
@@ -0,0 +1,80 @@
+import json
+import logging
+
+from . import config
+
+try:
+    import paho.mqtt.client as mqtt
+except ImportError:
+    MQTT_ENABLED = False
+else:
+    MQTT_ENABLED = config.MQTT_ENABLED
+
+
+logger = logging.getLogger(__name__)
+HA_STATUS_TOPIC = f"{config.MQTT_DISCOVERY_PREFIX}/status"
+HA_PREFIX = (
+    f"{config.MQTT_DISCOVERY_PREFIX}/sensor/{config.HOSTNAME}/{config.MQTT_OBJECT_ID}"
+)
+HA_TEMP_PREFIX = f"{HA_PREFIX}_temp"
+HA_CO2_PREFIX = f"{HA_PREFIX}_co2"
+EXPIRE_AFTER_SECONDS = 600
+
+
+def send_discovery_message(client: mqtt.Client):
+    device = {"identifiers": [config.HOSTNAME], "name": config.NAME}
+    temp_config = {
+        "device_class": "temperature",
+        "expire_after": EXPIRE_AFTER_SECONDS,
+        "icon": "mdi:temperature-celsius",
+        "state_topic": f"{HA_TEMP_PREFIX}/state",
+        "unit_of_measurement": "°C",
+        "unique_id": f"{config.MQTT_OBJECT_ID}_T",
+        "device": device,
+    }
+    co2_config = {
+        "device_class": "carbon_dioxide",
+        "expire_after": EXPIRE_AFTER_SECONDS,
+        "icon": "mdi:periodic-table-co2",
+        "state_topic": f"{HA_CO2_PREFIX}/state",
+        "unit_of_measurement": "ppm",
+        "unique_id": f"{config.MQTT_OBJECT_ID}_CO2",
+        "device": device,
+    }
+    client.publish(f"{HA_TEMP_PREFIX}/config", json.dumps(temp_config))
+    client.publish(f"{HA_CO2_PREFIX}/config", json.dumps(co2_config))
+
+
+def on_connect(client: mqtt.Client, *args, **kwargs):
+    send_discovery_message(client)
+    client.subscribe(HA_STATUS_TOPIC, qos=1)
+
+
+def handle_homeassistant_status(client: mqtt.Client, userdata, msg):
+    if msg.payload == "online":
+        send_discovery_message(client)
+
+
+def get_mqtt_client() -> mqtt.Client:
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.message_callback_add(
+        f"{config.MQTT_DISCOVERY_PREFIX}/status", handle_homeassistant_status
+    )
+    if config.MQTT_USERNAME:
+        client.username_pw_set(config.MQTT_USERNAME, config.MQTT_PASSWORD or None)
+    return client
+
+
+def send_co2_value(client: mqtt.Client, value: float):
+    client.publish(f"{HA_CO2_PREFIX}/state", value, retain=True)
+
+
+def send_temp_value(client: mqtt.Client, value: float):
+    client.publish(f"{HA_TEMP_PREFIX}/state", value, retain=True)
+
+
+def start_client(client: mqtt.Client):
+    """Blocking call to connect to the MQTT broker and loop forever"""
+    client.connect(config.MQTT_BROKER)
+    client.loop_forever(timeout=1.0, max_packets=1, retry_first_connection=False)
